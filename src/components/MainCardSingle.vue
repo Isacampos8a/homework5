a2df0af111af6edab0cf4c0589d6f3a487620265
Isabel Campos <isacampos8a@gmail.com>
2025-02-21 19:23:02 -0600

    changed bg color

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -10,7 +10,7 @@
 
 <template>
     <RouterLink :to="{name: 'CardDetail', params:{id: username}}">
-        <div class="border border-gray-500 rounded-lg shadow bg-gray-400 h-full">
+        <div class="border border-gray-500 rounded-lg shadow bg-gray-200 h-full">
             <img class="object-fill h-480 w-960 rounded-t-lg" v-bind:src="faker.image.urlLoremFlickr({category: 'cats', height:480, width:960})"/>
             <div class="p-4">
                 <p class="mb-2 test-2xl font-bold">{{ firstname }} {{ lastname }}</p>
